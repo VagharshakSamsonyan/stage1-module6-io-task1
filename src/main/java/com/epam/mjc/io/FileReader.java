e88dbfd95ed4b3f8cb2b15dcb3591b656bb09cb2
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-04-04 22:46:02 +0400

    3 of 3 tests passed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,49 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+            String line;
+            String name = null;
+            Integer age = null;
+            String email = null;
+            Long phone = null;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(": ");
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+
+                    switch (key) {
+                        case "Name":
+                            name = value;
+                            break;
+                        case "Age":
+                            age = Integer.parseInt(value);
+                            break;
+                        case "Email":
+                            email = value;
+                            break;
+                        case "Phone":
+                            phone = Long.parseLong(value);
+                            break;
+                    }
+                }
+            }
+
+            br.close();
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
